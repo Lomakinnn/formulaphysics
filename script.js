138820de8ecd95347192d888f44272e0860c9e23
Lomakinnn <christopherthef@gmail.com>
2024-05-25 04:28:34 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleSection(sectionId) {
+    var section = document.getElementById(sectionId);
+    if (section.style.display === "none" || section.style.display === "") {
+        section.style.display = "block";
+    } else {
+        section.style.display = "none";
+    }
+}
